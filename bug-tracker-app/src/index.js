9ef4f3cdb807b8ab3860a3f903d8e6b0cb93715c
Magesh <tkmagesh77@gmail.com>
2018-07-06 17:18:34 +0530

    Day-02 complete

--- bug-tracker-app/src/index.js
+++ bug-tracker-app/src/index.js
@@ -5,13 +5,23 @@ import BugTracker from './bugTracker';
 import Spinner from './spinner';
 import { Provider } from 'react-redux'
 import appStore from './store';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 
 ReactDOM.render(
-<Provider store={appStore}>
-	<div>
-		<Spinner />
-		<hr/>
-		<BugTracker />
-	</div>
-</Provider>,
-document.getElementById('root'));
+	<Provider store={appStore}>
+		<Router>
+			<div>
+				<p>
+				<Link to="/">Bug Tracker</Link>
+				<br/>
+				<Link to="/spinner">Spinner</Link>
+				<br />
+				</p>
+			  <Route exact path="/" component={BugTracker} />
+		      <Route path="/bugs" component={BugTracker} />
+		      <Route path="/spinner" component={Spinner} />
+		    </div> 
+	    </Router>
+	</Provider>
+	, document.getElementById('root')
+);
